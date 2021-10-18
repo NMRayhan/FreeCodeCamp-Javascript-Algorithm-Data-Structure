37b044eb9bb229c9615e106045fdae523bdfb464
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-19 01:34:13 +0600

    Record CollectionPassed

--- JS Basic/Script.js
+++ JS Basic/Script.js
@@ -1,21 +1,36 @@
-var myPlants = [
-    {
-      type: "flowers",
-      list: [
-        "rose",
-        "tulip",
-        "dandelion"
-      ]
+// Setup
+var recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
     },
-    {
-      type: "trees",
-      list: [
-        "fir",
-        "pine",
-        "birch"
-      ]
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
     }
-  ];
+  };
   
-  var secondTree = myPlants[1].list[1];
-  console.log(secondTree);
+  // Only change code below this line
+  function updateRecords(records, id, prop, value) {
+    if (prop !== 'tracks' && value !== "") {
+      records[id][prop] = value;
+    } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+      records[id][prop] = [value];
+    } else if (prop === "tracks" && value !== "") {
+      records[id][prop].push(value);
+    } else if (value === "") {
+      delete records[id][prop];
+    }
+    return records;
+  }
+  
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
