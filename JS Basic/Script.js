17857419ac1dd9ca02302e16847ada48a81e05d4
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-19 14:10:46 +0600

    Profile Lookup

--- JS Basic/Script.js
+++ JS Basic/Script.js
@@ -1,12 +1,43 @@
-function sum(arr, n) {
-    // Only change code below this line
-    var total = 0;
-    if (n <= 0) {
-        return n;
-    } else {
-        return sum(arr, n - 1) + arr[n - 1];
+// Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
     }
-    // Only change code above this line
+];
+
+
+function lookUpProfile(name, prop) {
+    for (let x = 0; x < contacts.length; x++) {
+        if (contacts[x].firstName === name) {
+            if (contacts[x].hasOwnProperty(prop)) {
+                return contacts[x][prop];
+            } else {
+                return "No such property";
+            }
+        }
+    }
+    return "No such contact";
 }
 
-console.log(sum([1, 2, 3, 4], 2));
+console.log(lookUpProfile("Akira", "likes"));
