a6a7ea178fa7916bc81b72cb9246c1b1da609413
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-19 19:35:30 +0600

    Use Arrow Functions to Write Concise Anonymous Functions

--- JS Basic/ES6.js
+++ JS Basic/ES6.js
@@ -1,30 +1,21 @@
-let obj = {
-    name: "freeCodeCamp",
-    review: "Awesome"
+const myFun = function () {
+    const myVar = "Value";
+    return myVar;
+}
+console.log(myFun());
+
+const sumOfTwoInt = (num1, num2) => {
+    const myVar = num1 + num2;
+    return myVar;
+}
+console.log(sumOfTwoInt(1, 2));
+
+const sum = (num1, num2) => num1 + num2;
+
+console.log(sum(2, 5));
+
+const magic = () => {
+    return new Date();
 };
-console.log(obj);
-obj.name = "w3School";
-console.log(obj);
 
-Object.freeze(obj);
-obj.name = "freeCodeCamp";
-console.log(obj);
-
-
-function freezeObj() {
-    const MATH_CONSTANTS = {
-      PI: 3.14
-    };
-    // Only change code below this line
-    Object.freeze(MATH_CONSTANTS);
-    // Only change code above this line
-    try {
-      MATH_CONSTANTS.PI = 99;
-    } catch (ex) {
-      console.log(ex);
-    }
-    return MATH_CONSTANTS.PI;
-  }
-  const PI = freezeObj();
-
-//   Showing Error: [TypeError: Cannot assign to read only property 'PI' of object '#<Object>']
+console.log(magic());
