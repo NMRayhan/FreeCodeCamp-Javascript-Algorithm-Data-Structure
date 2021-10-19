68861033aa6275daf1ba76b1f64e0d2baf73379b
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-19 15:06:18 +0600

    Use Recursion to Create a CountdownPassed

--- JS Basic/Script.js
+++ JS Basic/Script.js
@@ -1,12 +1,31 @@
-// Only change code below this line
 function countdown(n) {
     if (n < 1) {
-      return [];
+        return [];
     } else {
-      var arr = countdown(n - 1)
-      arr.unshift(n)
-      return arr;
+        var arr = countdown(n - 1)
+        arr.unshift(n)
+        return arr;
     }
-  }
-  // Only change code above this line
-  console.log(countdown(10));
+}
+console.log(countdown(10));
+
+function countdown(n) {
+    if (n < 1) {
+        return [];
+    } else {
+        const arr = countdown(n - 1);
+        arr.splice(0, 0, n);
+        return arr;
+    }
+}
+console.log(countdown(11));
+
+function countdown(n) {
+    return n < 1 ? [] : [n].concat(countdown(n - 1));
+}
+console.log(countdown(12));
+
+function countdown(n) {
+    return n < 1 ? [] : [n, ...countdown(n - 1)];
+}
+console.log(countdown(7));
