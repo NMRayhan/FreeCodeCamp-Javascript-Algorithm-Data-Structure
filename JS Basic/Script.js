a156e9a0ee1d20283ef2bb7daed0c4ef804696a9
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-19 14:15:23 +0600

    Generate Random Fractions with JavaScript

--- JS Basic/Script.js
+++ JS Basic/Script.js
@@ -1,43 +1,10 @@
-// Setup
-var contacts = [
-    {
-        "firstName": "Akira",
-        "lastName": "Laine",
-        "number": "0543236543",
-        "likes": ["Pizza", "Coding", "Brownie Points"]
-    },
-    {
-        "firstName": "Harry",
-        "lastName": "Potter",
-        "number": "0994372684",
-        "likes": ["Hogwarts", "Magic", "Hagrid"]
-    },
-    {
-        "firstName": "Sherlock",
-        "lastName": "Holmes",
-        "number": "0487345643",
-        "likes": ["Intriguing Cases", "Violin"]
-    },
-    {
-        "firstName": "Kristian",
-        "lastName": "Vos",
-        "number": "unknown",
-        "likes": ["JavaScript", "Gaming", "Foxes"]
-    }
-];
+function randomFraction() {
 
+    // Only change code below this line
+  
+    return Math.ceil(Math.random()*100);
+  
+    // Only change code above this line
+  }
 
-function lookUpProfile(name, prop) {
-    for (let x = 0; x < contacts.length; x++) {
-        if (contacts[x].firstName === name) {
-            if (contacts[x].hasOwnProperty(prop)) {
-                return contacts[x][prop];
-            } else {
-                return "No such property";
-            }
-        }
-    }
-    return "No such contact";
-}
-
-console.log(lookUpProfile("Akira", "likes"));
+  console.log(randomFraction());
