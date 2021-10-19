6a653c12107f841d2b1057a3991369887e830505
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-19 14:56:20 +0600

    Use Recursion to Create a Countdown

--- JS Basic/Script.js
+++ JS Basic/Script.js
@@ -1,16 +1,12 @@
-function checkSign1(num) {
-    if (num === 0) {
-        return "zero"
-    } else if (num > 0) {
-        return "positive";
+// Only change code below this line
+function countdown(n) {
+    if (n < 1) {
+      return [];
     } else {
-        return "negative";
+      var arr = countdown(n - 1)
+      arr.unshift(n)
+      return arr;
     }
-}
-
-console.log(checkSign1(-1));
-
-function checkSign(num) {
-    return (num === 0) ? "zero" : (num > 0) ? "positive" : "negative";
-}
-console.log(checkSign(-1));
+  }
+  // Only change code above this line
+  console.log(countdown(10));
