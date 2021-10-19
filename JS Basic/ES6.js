cadee571924ac6ee19ab65f761dabe1616f2fea4
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-19 23:19:28 +0600

    Use the Rest Parameter with Function Parameters

--- JS Basic/ES6.js
+++ JS Basic/ES6.js
@@ -14,3 +14,18 @@ console.log(myConcat([1, 2], [3, 4, 5]));
 const increment = (number = 1, value = 1) => number + value;
 // Only change code above this line
 console.log(increment(5));
+
+function howMany(...args) {
+    return "You have passed " + args.length + " arguments.";
+}
+console.log(howMany(0, 1, 2));
+console.log(howMany("string", null, [1, 2, 3], {}));
+
+
+const sum = (...args) => {
+    return args.reduce((previous, current) => {
+        return previous + current;
+    }, 0);
+}
+
+console.log(sum(1, 2, 3, 4, 5));
