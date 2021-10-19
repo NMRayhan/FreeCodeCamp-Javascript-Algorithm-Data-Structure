1eeb54e0c6bc5825d0f31b30f73d8e73c7361f4f
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-19 14:22:22 +0600

    Generate Random Whole Numbers within a Range

--- JS Basic/Script.js
+++ JS Basic/Script.js
@@ -1,10 +1,10 @@
-function randomFraction() {
+function randomRange(myMin, myMax) {
 
     // Only change code below this line
 
-    return Math.floor(Math.random() * 10);
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
 
     // Only change code above this line
 }
 
-console.log(randomFraction());
+console.log(randomRange(2,10));
