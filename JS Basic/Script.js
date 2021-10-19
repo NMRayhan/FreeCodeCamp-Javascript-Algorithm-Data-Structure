3e742f8c33f8599700481a163e9bd39524c459ae
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-19 14:17:40 +0600

    Generate Random Whole Numbers with JavaScript

--- JS Basic/Script.js
+++ JS Basic/Script.js
@@ -1,10 +1,10 @@
 function randomFraction() {
 
     // Only change code below this line
-  
-    return Math.ceil(Math.random()*100);
-  
-    // Only change code above this line
-  }
 
-  console.log(randomFraction());
+    return Math.floor(Math.random() * 10);
+
+    // Only change code above this line
+}
+
+console.log(randomFraction());
