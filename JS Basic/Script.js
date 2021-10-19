0aa20c833be7d67a42166e886ffba240a4149985
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-19 14:23:58 +0600

    Use the parseInt Function

--- JS Basic/Script.js
+++ JS Basic/Script.js
@@ -1,10 +1,4 @@
-function randomRange(myMin, myMax) {
-
-    // Only change code below this line
-
-    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin;
-
-    // Only change code above this line
-}
-
-console.log(randomRange(2,10));
+function convertToInteger(str) {
+    return parseInt(str)
+  }
+  convertToInteger("56");
