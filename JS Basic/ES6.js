bb6067023fa7329559869f795e453bc0dbcd2139
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-20 21:47:38 +0600

    Use Destructuring Assignment to Pass an Object as a Function's Parameters

--- JS Basic/ES6.js
+++ JS Basic/ES6.js
@@ -7,3 +7,18 @@ function removeFirstTwo(list) {
 }
 const arr = removeFirstTwo(source);
 console.log(arr);
+
+const personOne = {
+    name: "Towfiq Elahi Plabon",
+    age: 28,
+    address: {
+        city: "JoypureHat",
+        States: "Rajshahi"
+    }
+}
+
+const person = ({name, age, address:{city, States}}) =>{
+    return `My Name is ${name}, I'm ${age}'s Old and i'm from ${city}, it's States of ${States}`;
+}
+
+console.log(person(personOne));
