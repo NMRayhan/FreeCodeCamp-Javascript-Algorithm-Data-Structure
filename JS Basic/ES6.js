1142e180610eeae9691f981e5d3da7fe44dd4d4d
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-21 01:37:02 +0600

    Create Strings using Template Literals

--- JS Basic/ES6.js
+++ JS Basic/ES6.js
@@ -1,24 +1,17 @@
-const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-function removeFirstTwo(list) {
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+    const failureItems = []
 
-    const [a, b, ...arr] = list;
-
-    return arr;
-}
-const arr = removeFirstTwo(source);
-console.log(arr);
-
-const personOne = {
-    name: "Towfiq Elahi Plabon",
-    age: 28,
-    address: {
-        city: "JoypureHat",
-        States: "Rajshahi"
+    for (let i = 0; i < arr.length; i++) {
+        failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
     }
+
+    return failureItems;
 }
 
-const person = ({name, age, address:{city, States}}) =>{
-    return `My Name is ${name}, I'm ${age}'s\nOld and i'm from ${city}, it's States of ${States}`;
-}
-
-console.log(person(personOne));
+const failuresList = makeList(result.failure);
+console.log(failuresList);
