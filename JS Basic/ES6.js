2a672699d6085e617edcbcf708e62f6dbcb6dbb6
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-20 18:56:53 +0600

    Use Destructuring Assignment to Extract Values from Objects

--- JS Basic/ES6.js
+++ JS Basic/ES6.js
@@ -1,19 +1,10 @@
-let str = "Apple, Banana, Kiwi";
-console.log(str.slice(6, 13));
-console.log(str.slice(-12, -6));
+const HIGH_TEMPERATURE ={
+    yesterday : 77,
+    today: 75,
+    tomorrow: 80
+};
 
-console.log(str.substring(6, 13));
-
-
-let str2 = "Daffodil International University";
-
-for (let i = 0; i < str.length; i+=1){
-    console.log(str2.substr(i));
-}
-
-let text = "Please visit Microsoft and Microsoft!";
-let newText = text.replace("Microsoft", "W3Schools");
-console.log(newText);
-
-let newText2 = text.replace(/MICROSOFT/i,"W3Schools")
-console.log(newText2);
+const {today, tomorrow, yesterday} = HIGH_TEMPERATURE;
+console.log(today);
+console.log(tomorrow);
+console.log(yesterday);
