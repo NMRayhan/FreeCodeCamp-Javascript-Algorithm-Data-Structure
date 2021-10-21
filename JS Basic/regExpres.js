eb1f6f94e839c6d80c3a0c4df4f70870b1aacfbe
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-21 17:59:40 +0600

    Match Anything with Wildcard Period

--- JS Basic/regExpres.js
+++ JS Basic/regExpres.js
@@ -1,4 +1,7 @@
-let twinkleStar = "Twinkle, twinkle, little star";
-let starRegex = /Twinkle/gi; // Change this line
-let result = twinkleStar.match(starRegex); // Change this line
+let myString ="Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi qui nobis unde aspernatur nisi maiores quis animi veniam! Impedit tempora fugit quasi ipsam officiis et eius similique, non sit, ab veniam laborum quas odio! Tempore error eligendi libero tenetur quas!";
+
+let searchStr = /ab./gi; //using Wildcard Period .
+
+let result = myString.match(searchStr);
 console.log(result);
+console.log(result.length);
