ecd05a4a89cf105c92e2831c40417d1d77b6854f
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-21 15:52:06 +0600

    Complete a Promise with resolve and reject

--- JS Basic/ES6.js
+++ JS Basic/ES6.js
@@ -1,26 +1,10 @@
-(function () {
-    // We keep these variables private inside this closure scope
-
-    var myGrades = [93, 95, 88, 0, 55, 91];
-
-    var average = function () {
-        var total = myGrades.reduce(function (accumulator, item) {
-            return accumulator + item
-        }, 0);
-
-        return 'Your average grade is ' + total / myGrades.length + '.';
+const makeServerRequest = new Promise((resolve, reject) => {
+    // responseFromServer represents a response from a server
+    let responseFromServer;
+      
+    if(responseFromServer) {
+      resolve ("We got the data");
+    } else {  
+      reject("Data not received");
     }
-
-    var failing = function () {
-        var failingGrades = myGrades.filter(function (item) {
-            return item < 70;
-        });
-
-        return 'You failed ' + failingGrades.length + ' times.';
-    }
-
-    console.log(failing());
-    console.log(myGrades);
-
-}());
-
+  });
