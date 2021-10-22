ee279bb660900b0abf3ce6fefbcebf330ebee0b5
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-22 20:07:24 +0600

    Match Letters of the Alphabet

--- JS Basic/regExpres.js
+++ JS Basic/regExpres.js
@@ -1,5 +1,5 @@
-let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
-let vowelRegex = /[aeiou]/gi; // Change this line
+let quoteSample = "Beuare of Beware in Beiare the Beabove  uare code; I have only proved it correct, not tried it.";
+let vowelRegex = /be[uwia]/gi; // Change this line
 let result = quoteSample.match(vowelRegex); // Change this line
 
 console.log(result);
