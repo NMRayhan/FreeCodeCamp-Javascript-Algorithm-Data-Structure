88b016db843e02ac810050cbc98fd3f614923b1f
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-22 20:27:56 +0600

    Neglact Character

--- JS Basic/regExpres.js
+++ JS Basic/regExpres.js
@@ -1,4 +1,4 @@
 let quoteSample = "3 blind mice.";
 let myRegex = /[^aeiou0-9]/gi; // Change this line
 let result = quoteSample.match(myRegex);
-console.log(result); //regex myRegex should match 9 items
+console.log(result); //regex myRegex should match 9 item
