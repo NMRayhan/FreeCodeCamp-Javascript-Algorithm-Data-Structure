7a0dd6c4cc069959e4a2f801dab577aa7b8385c1
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-23 17:19:01 +0600

    Match Everything But Letters and Numbers

--- JS Basic/regExpres.js
+++ JS Basic/regExpres.js
@@ -1,4 +1,4 @@
-let quoteSample = "Pack my box with five dozen liquor jugs";
-let alphabetRegexV2 = /\w/g; // Change this line \w. this is the Shortcut is Equals to [A-Za-z0-9_]
+let quoteSample = "Coding! 42%";
+let alphabetRegexV2 = /[^A-Za-z0-9_]/g; // \W. or Uppercase W this is the Shortcut is Equals to [^A-Za-z0-9_]
 let result = quoteSample.match(alphabetRegexV2).length;
 console.log(result);
