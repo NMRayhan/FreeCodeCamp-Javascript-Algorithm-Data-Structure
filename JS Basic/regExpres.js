b18439c22d6c5d4f76cdce03d0ce6f6c855717e2
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-23 22:25:35 +0600

    Space Remover in first and Last in Sentence

--- JS Basic/regExpres.js
+++ JS Basic/regExpres.js
@@ -1,4 +1,4 @@
-let str = "one two three";
-let fixRegex = /(\w+)\s(\w+)\s(\w+)/;
-let replaceText = '$3 $2 $1'; 
-let result = str.replace(fixRegex,replaceText);
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g;
+let result = hello.replace(wsRegex, "");
+console.log(result);
