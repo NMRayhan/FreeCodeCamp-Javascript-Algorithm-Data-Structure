91dbf7cec593578250972b6139e32e38b230df18
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-23 17:52:14 +0600

    Specify Upper and Lower Number of Matches

--- JS Basic/regExpres.js
+++ JS Basic/regExpres.js
@@ -1,7 +1,7 @@
 /*Search for non-whitespace using \S, which is an uppercase s. This pattern will not match whitespace, carriage return, tab, form feed, and new line characters. You can think of it being similar to the character class [^ \r\t\f\n\v].*/
 
 
-let sample = "Whitespace is important in separating words";
-let countWhiteSpace = /\S/g; // Change this line
-let result = sample.match(countWhiteSpace);
+let ohStr = "Ohhhh no";
+let ohRegex = /oh{3,6}\sno/gi; // Change this line
+let result = ohRegex.test(ohStr);
 console.log(result);
