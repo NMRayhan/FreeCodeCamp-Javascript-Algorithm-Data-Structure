34aeba1c03461522db3b1bb8ce20b1a2929ca639
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-23 17:25:05 +0600

    Match All Non-Numbers

--- JS Basic/regExpres.js
+++ JS Basic/regExpres.js
@@ -1,4 +1,4 @@
 let quoteSample = "Coding! 42%";
-let alphabetRegexV2 = /\d/g; // lowercase d for digit, this is equal to the Character class [0-9]
+let alphabetRegexV2 = /\D/g; // Uppercase D for non-digit character, this is equal to the Character class [^0-9]
 let result = quoteSample.match(alphabetRegexV2).length;
 console.log(result);
