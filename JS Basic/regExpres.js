bcd36a207a617af2bf5d38cf4249c84decc2cac1
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-23 14:31:26 +0600

    Find Characters with Lazy Matching

--- JS Basic/regExpres.js
+++ JS Basic/regExpres.js
@@ -1,4 +1,4 @@
-let difficultSpelling = "Mississippi";
-let myRegex = /s+/gi; 
-let result = difficultSpelling.match(myRegex);
+let chewieQuote = "<h1>Winter is coming</h1>";
+let chewieRegex = /<.*?>/;
+let result = chewieQuote.match(chewieRegex);
 console.log(result); 
