f83c70e3972a96b433b2420dbdc4808927b988ea
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-23 17:40:44 +0600

    Match Non-Whitespace Characters

--- JS Basic/regExpres.js
+++ JS Basic/regExpres.js
@@ -1,7 +1,7 @@
-/*You can search for whitespace using \s, which is a lowercase s. This pattern not only matches whitespace, but also carriage return, tab, form feed, and new line characters. You can think of it as similar to the character class [ \r\t\f\n\v].*/
+/*Search for non-whitespace using \S, which is an uppercase s. This pattern will not match whitespace, carriage return, tab, form feed, and new line characters. You can think of it being similar to the character class [^ \r\t\f\n\v].*/
 
 
 let sample = "Whitespace is important in separating words";
-let countWhiteSpace = /\s/g; // Change this line
+let countWhiteSpace = /\S/g; // Change this line
 let result = sample.match(countWhiteSpace);
 console.log(result);
