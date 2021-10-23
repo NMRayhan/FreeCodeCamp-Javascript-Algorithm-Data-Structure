a1684d27b140811dd15d6e2afe2ce18cf265ac2c
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-23 18:46:03 +0600

    Positive and Negative Lookahead

--- JS Basic/regExpres.js
+++ JS Basic/regExpres.js
@@ -1,10 +1,6 @@
-/*To specify a certain number of patterns, just have that one number between the curly brackets.
+/*total 6 character and last 2 character is Digit*/
 
-For example, to match only the word hah with the letter a 3 times, your regex would be /ha{3}h/.*/
-
-let A4 = "haaaah";
-let A2 = "haah";
-let A10 = "h" + "a".repeat(10) + "h";
-let multipleA = /ha{2}h/;
-let result = multipleA.test(A4);
+let sampleWord = "banana12";
+let pwRegex = /(?=\w{6})(?=\D*\d{2})/gi; // Change this line
+let result = pwRegex.test(sampleWord);
 console.log(result);
