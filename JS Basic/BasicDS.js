da07f219d4fb7c41099d9029a9376016bc64267c
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-24 15:58:23 +0600

    Add Items to an Array with push() and unshift()

--- JS Basic/BasicDS.js
+++ JS Basic/BasicDS.js
@@ -1,5 +1,9 @@
-let myArray = ["a", "b", "c", "d"];
-// Only change code below this line
-myArray[1] = "anything we want";
-// Only change code above this line
-console.log(myArray);
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift('I', 2, 'three');
+    arr.push(7, 'VIII', 9);
+    // Only change code above this line
+    return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
