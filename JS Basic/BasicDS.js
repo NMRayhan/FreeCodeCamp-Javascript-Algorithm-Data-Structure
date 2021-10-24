8774b81bb453732f7c8173aee642b2938db48f16
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-24 23:39:00 +0600

    Combine Arrays with the Spread Operator

--- JS Basic/BasicDS.js
+++ JS Basic/BasicDS.js
@@ -1,14 +1,11 @@
 /*
-We have defined a function, copyMachine which takes arr (an array) and num (a number) as arguments. The function is supposed to return a new array made up of num copies of arr. We have done most of the work for you, but it doesn't work quite right yet. Modify the function using spread syntax so that it works correctly (hint: another method we have already covered might come in handy here!)
+We have defined a function spreadOut that returns the variable sentence. Modify the function using the spread operator so that it returns the array ['learning', 'to', 'code', 'is', 'fun'].
 */
 
-function copyMachine(arr, num) {
-    let newArr = [];
-    while (num >= 1) {
-        newArr.push([...arr])
-        num--;
-    }
-    return newArr;
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ["learning", ...fragment, "is", "fun"];
+    return sentence;
 }
 
-console.log(copyMachine([1, 2, 3], 5));
+console.log(spreadOut());
