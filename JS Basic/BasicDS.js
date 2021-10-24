a220576026d1b27f5b7161b5ff4e2c1f6ed182d5
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-24 23:34:50 +0600

    Copy an Array with the Spread Operator

--- JS Basic/BasicDS.js
+++ JS Basic/BasicDS.js
@@ -1,10 +1,14 @@
 /*
-We have defined a function, forecast, that takes an array as an argument. Modify the function using slice() to extract information from the argument array and return a new array that contains the string elements warm and sunny.
+We have defined a function, copyMachine which takes arr (an array) and num (a number) as arguments. The function is supposed to return a new array made up of num copies of arr. We have done most of the work for you, but it doesn't work quite right yet. Modify the function using spread syntax so that it works correctly (hint: another method we have already covered might come in handy here!)
 */
 
-function forecast(arr) {
-    let newArr = arr.slice(2,4);
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+        newArr.push([...arr])
+        num--;
+    }
     return newArr;
 }
 
-console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+console.log(copyMachine([1, 2, 3], 5));
