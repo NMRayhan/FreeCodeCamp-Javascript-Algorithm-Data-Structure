f6277f36ca1b32e0392a89d27af28cf2e61f7e60
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-24 23:07:03 +0600

    Copy Array Items Using slice()

--- JS Basic/BasicDS.js
+++ JS Basic/BasicDS.js
@@ -1,12 +1,10 @@
 /*
-We have defined a function, htmlColorNames, which takes an array of HTML colors as an argument. Modify the function using splice() to remove the first two elements of the array and add 'DarkSalmon' and 'BlanchedAlmond' in their respective places.
-*/ 
+We have defined a function, forecast, that takes an array as an argument. Modify the function using slice() to extract information from the argument array and return a new array that contains the string elements warm and sunny.
+*/
 
-function htmlColorNames(arr) {
-    let startIndex = 0;
-    let amountOfDelete = 2;
-    arr.splice(startIndex, amountOfDelete, 'DarkSalmon', 'BlanchedAlmond')
-    return arr;
+function forecast(arr) {
+    let newArr = arr.slice(2,4);
+    return newArr;
 }
 
-console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
