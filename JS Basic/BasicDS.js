056608ac48319cbcdbef6829ca715431c3162306
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-25 15:32:38 +0600

    Create complex multi-dimensional arrays

--- JS Basic/BasicDS.js
+++ JS Basic/BasicDS.js
@@ -1,15 +1,19 @@
 /*
-We have defined a function, filteredArray, which takes arr, a nested array, and elem as arguments, and returns a new array. elem represents an element that may or may not be present on one or more of the arrays nested within arr. Modify the function, using a for loop, to return a filtered version of the passed array such that any array nested within arr containing elem has been removed.
+We have defined a variable, myNestedArray, set equal to an array. Modify myNestedArray, using any combination of strings, numbers, and booleans for data elements, so that it has exactly five levels of depth (remember, the outer-most array is level 1). Somewhere on the third level, include the string deep, on the fourth level, include the string deeper, and on the fifth level, include the string deepest.
 */
 
-function filteredArray(arr, elem) {
-    let newArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i].indexOf(elem) == -1){
-            newArr.push(arr[i]);
-        }
-    }
-    return newArr;
-}
+let myNestedArray = [
+    ["unshift", false, 1, 2, 3, "complex", "nested"],
+    ["loop", "shift", 6, 7, 1000, "method"],
+    ["concat", false, true, "spread", "array", ["deep"]],
+    ["mutate", 1327.98, "splice", "slice", "push", [["deeper"]]],
+    ["iterate", 1.3849, 7, "8.4876", "arbitrary", "depth", [[["deepest"]]]]
+];
 
-console.log(filteredArray([["trumpets", 2], ["flutes", 4], ["saxophones", 2]], 2));
+let nestedNewArray = [
+    'level 1',                      /* myNestedArray[0]             */
+    ['level 2'],	               /* myNestedArray[1][0]          */
+    [['level 3', 'deep']],	       /* myNestedArray[2][0][0]       */
+    [[['level 4', 'deeper']]],    /* myNestedArray[3][0][0][0]    */
+    [[[['level 5', 'deepest']]]]  /* myNestedArray[4][0][0][0][0] */
+];
