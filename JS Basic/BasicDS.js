8b1d24ca880f5faf6c9247451d0efeec9a95ea26
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-25 23:19:37 +0600

    Modify an Array Stored in an Object

--- JS Basic/BasicDS.js
+++ JS Basic/BasicDS.js
@@ -1,28 +1,31 @@
 /*
-Finish writing the getArrayOfUsers function so that it returns an array containing all the properties in the object it receives as an argument.
+Take a look at the object we've provided in the code editor. The user object contains three keys. The data key contains five keys, one of which contains an array of friends. From this, you can see how flexible objects are as data structures. We've started writing a function addFriend. Finish writing it so that it takes a user object and adds the name of the friend argument to the array stored in user.data.friends and returns that array.
 */
 
-let users = {
-    Alan: {
-        age: 27,
-        online: false
-    },
-    Jeff: {
-        age: 32,
-        online: true
-    },
-    Sarah: {
-        age: 48,
-        online: false
-    },
-    Ryan: {
-        age: 19,
-        online: true
+let user = {
+    name: 'Kenneth',
+    age: 28,
+    data: {
+        username: 'kennethCodesAllDay',
+        joinDate: 'March 26, 2016',
+        organization: 'freeCodeCamp',
+        friends: [
+            'Sam',
+            'Kira',
+            'Tomo'
+        ],
+        location: {
+            city: 'San Francisco',
+            state: 'CA',
+            country: 'USA'
+        }
     }
 };
 
-function getArrayOfUsers(usersObj) {
-    return Object.keys(usersObj);
+function addFriend(userObj, friend) {
+    userObj.data.friends.push(friend);
+    return userObj.data.friends
 }
 
-console.log(getArrayOfUsers(users));
+console.log(addFriend(user, 'Pete'));
+
