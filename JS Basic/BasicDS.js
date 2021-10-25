e37eed36b083d69b0541c829e844c81860120b31
NMRayhan <nurmdrayhan97@gmail.com>
2021-10-25 17:44:38 +0600

    Modify an Object Nested Within an Object

--- JS Basic/BasicDS.js
+++ JS Basic/BasicDS.js
@@ -1,14 +1,16 @@
 /*
-A foods object has been created with three entries. Using the syntax of your choice, add three more entries to it: bananas with a value of 13, grapes with a value of 35, and strawberries with a value of 27.
+Here we've defined an object userActivity, which includes another object nested within it. Set the value of the online key to 45.
 */
 
-let foods = {
-    apples: 25,
-    oranges: 32,
-    plums: 28
+let userActivity = {
+    id: 23894201352,
+    date: 'January 1, 2017',
+    data: {
+        totalUsers: 51,
+        online: 42
+    }
 };
-foods.banana = 13;
-foods["grapes"] = 35;
-const specialFood = "strawberries"
-foods[specialFood] = 27;
-console.log(foods);
+
+userActivity.data.online = 45;
+
+console.log(userActivity);
