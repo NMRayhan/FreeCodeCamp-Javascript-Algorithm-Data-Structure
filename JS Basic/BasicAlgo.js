6dbf41d98c351c21aa2e174bd658161af6c3597a
NM_Rayhan <nurmdrayhan97@gmail.com>
2021-10-28 12:34:17 +0600

    Reverse a String

--- JS Basic/BasicAlgo.js
+++ JS Basic/BasicAlgo.js
@@ -1,10 +1,16 @@
 /*
-You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature. Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.
+Reverse the provided string.
+You may need to turn the string into an array before you can reverse it.
+Your result must be a string.
+
+Array.split() Method create a Array From String Separated By ""(comma)
+Array.reverse() Method use Reverse and Return as New Array
+Array.join() Method use for Join Separated Array by ""(empty String) and return as new String
+
 */
 
-function convertToF(celsius) {
-    let fahrenheit = celsius*(9/5)+32;
-    return fahrenheit;
+function reverseString(str) {
+    return str.split("").reverse().join("");
   }
   
-  convertToF(30);
+console.log(reverseString("hello"));
