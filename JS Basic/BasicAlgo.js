296a4d3044fb0d65d7fb3bef05905056bac02f08
NM_Rayhan <nurmdrayhan97@gmail.com>
2021-10-29 22:23:12 +0600

    Return Largest Numbers in Arrays

--- JS Basic/BasicAlgo.js
+++ JS Basic/BasicAlgo.js
@@ -1,17 +1,21 @@
 /*
-Return the length of the longest word in the provided sentence.
-Your response should be a number.
+Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
 */
 
-function findLongestWordLength(str) {
-    let separetStr = str.split(" ");
-    let newStr = "";
-    for (let i = 0; i < separetStr.length; i++) {
-        if (separetStr[i].length > newStr.length){
-            newStr = separetStr[i];
+function largestOfFour(arr) {
+    let maxNum = 0;
+    let maxArr = []
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (maxNum < arr[i][j]) {
+                maxNum = arr[i][j];
+            }
+            maxArr[i] = maxNum;
         }
+        maxNum = 0;
     }
-    return newStr.length;
+    return maxArr;
 }
 
-console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]));
