8d0721f3e030a936240ebd1c46da2133d84bcf02
NMRayhan <nmrayhan97@gmail.com>
2021-11-20 12:34:11 +0600

    Repeat a String Repeat a String

--- JS Basic/BasicAlgo.js
+++ JS Basic/BasicAlgo.js
@@ -1,17 +1,14 @@
 /*
-Check if a string (first argument, str) ends with the given target string (second argument, target).
-This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
 */
 
 
-function confirmEnding(str, target) {
-    let tarLength = target.length;
-    let newStr = str.substring(str.length - tarLength);
-    if(target == newStr){
-        return true
-    }else{
-        return false
+function repeatStringNumTimes(str, num) {
+    let newAns = ""
+    for(let count=0; count<num; count++){
+        newAns = newAns + str;
     }
+    return newAns;
 }
 
-confirmEnding("Bastian", "n");
+console.log(repeatStringNumTimes("*", 3));
