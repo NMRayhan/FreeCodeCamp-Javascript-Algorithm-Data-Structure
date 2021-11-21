99d64997bb9a62b0d1bf110fe71e7739908dbab7
Nur Mohammad Rayhan <nurmdrayhan97@gmail.com>
2021-11-21 18:28:20 +0600

    Demo commit 2

--- JS Basic/BasicAlgo.js
+++ JS Basic/BasicAlgo.js
@@ -1,9 +1,9 @@
 /*
 Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
 */
-
 function findElement(arr, func) {
     let num = 0;
+
     console.log(func);
     return num;
 }
