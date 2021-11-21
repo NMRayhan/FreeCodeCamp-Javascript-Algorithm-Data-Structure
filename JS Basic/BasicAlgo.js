f7d2c7d6ab6e1e4950fb23be19b646f04c23731c
Nur Mohammad Rayhan <nurmdrayhan97@gmail.com>
2021-11-21 23:37:17 +0600

    Title Case a Sentence

--- JS Basic/BasicAlgo.js
+++ JS Basic/BasicAlgo.js
@@ -1,16 +1,18 @@
 /*
-Check if a value is classified as a boolean primitive. Return true or false.
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+For the purpose of this exercise, you should also capitalize connecting words like the and of.
 */
 
 
-function booWho(bool) {
-    if (bool === true) {
-        return true;
-    } else if (bool === false) {
-        return true;
-    } else {
-        return false;
+function titleCase(str) {
+    let newArr = str.split(" ")
+    let newStr = " "
+    for (let i = 0; i < newArr.length; i++) {
+        let upperStr = newArr[i].toLowerCase();
+        let newUpperStr = upperStr.charAt(0).toUpperCase() + upperStr.slice(1);
+        newStr = newStr + newUpperStr+" ";
     }
+    return newStr;
 }
 
-console.log(booWho(false));
+console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"));
