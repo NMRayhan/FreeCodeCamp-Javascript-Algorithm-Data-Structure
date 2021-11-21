c5b5b0b13c89c356b954eb6bc6d8dfa1f53a3edb
Nur Mohammad Rayhan <nurmdrayhan97@gmail.com>
2021-11-21 19:42:23 +0600

    Boo who

--- JS Basic/BasicAlgo.js
+++ JS Basic/BasicAlgo.js
@@ -1,18 +1,16 @@
 /*
-Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.
+Check if a value is classified as a boolean primitive. Return true or false.
 */
 
 
-function findElement(arr, func) {
-    let num = 0;
-
-    for(let i=0; i<arr.length; i++){
-        num = arr[i];
-        if(func(num)){
-            return num;
-        }
+function booWho(bool) {
+    if (bool === true) {
+        return true;
+    } else if (bool === false) {
+        return true;
+    } else {
+        return false;
     }
-    return undefined;
 }
 
-console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
+console.log(booWho(false));
